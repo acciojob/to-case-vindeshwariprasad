fbe5740c7ef044a70ec7bba7acad0dc9d567b216
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 05:03:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function toCase(text) {
+function toCase(str) {
+	if (!str) {
+        return '-';
+    }
+    
+    // Convert the string to lowercase and uppercase with '-' as the delimiter
+    const lowerCase = str.toLowerCase();
+    const upperCase = str.toUpperCase();
+    
+    // Return the combined string with '-' as the delimiter
+    return lowerCase + '-' + upperCase;
   // write your code here
 }
 
